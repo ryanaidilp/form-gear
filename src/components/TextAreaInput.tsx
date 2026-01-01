05c34dccdd3844da8d861559c098dc4747ddf908
Fajrian Aidil Pratama <fajrianaidilp@gmail.com>
2026-01-01 10:48:31 +0700

    fix(ui): prevent remark button overlapping on mobile
    Change layout from CSS Grid to Flexbox for better responsive behavior.
    Replaced grid-cols-12/col-span-11 pattern with flex/flex-1/shrink-0.

--- src/components/TextAreaInput.tsx
+++ src/components/TextAreaInput.tsx
@@ -17,8 +17,8 @@ const TextAreaInput: FormComponentBase = props => {
 
   return (
     <div class="grid md:grid-cols-3 border-b border-gray-300/[.40] dark:border-gray-200/[.10] p-2">
-      <div class="font-light text-sm space-y-2 py-2.5 px-2">        
-        <div class="inline-flex space-x-2"> 
+      <div class="font-light text-sm space-y-2 py-2.5 px-2">
+        <div class="inline-flex space-x-2">
           <div innerHTML={props.component.label} />
           <Show when={props.component.required}>
             <span class="text-pink-600">*</span>
@@ -38,14 +38,10 @@ const TextAreaInput: FormComponentBase = props => {
           </Show>
         </div>
       </div>
-      <div class="font-light text-sm space-x-2 py-2.5 px-2 md:col-span-2 grid grid-cols-12">
-        <div class=""
-             classList={{
-              'col-span-11 lg:-mr-4' : enableRemark(),
-              'col-span-12' : !(enableRemark()),
-             }}  >
+      <div class="font-light text-sm py-2.5 px-2 md:col-span-2 flex items-start gap-2">
+        <div class="flex-1">
           <Show when={props.component.lengthInput === undefined}>
-            <textarea value={props.value} 
+            <textarea value={props.value}
                 rows={props.component.rows || 2}
                 class={ classInput }
                 classList={{
@@ -60,7 +56,7 @@ const TextAreaInput: FormComponentBase = props => {
           />
           </Show>
           <Show when={props.component.lengthInput !== undefined && props.component.lengthInput.length > 0}>
-            <textarea value={props.value} 
+            <textarea value={props.value}
                 rows={props.component.rows || 2}
                 class={ classInput }
                 classList={{
@@ -79,30 +75,30 @@ const TextAreaInput: FormComponentBase = props => {
           <Show when={props.validationMessage?.length > 0}>
             <For each={props.validationMessage}>
               {(item:any) => (
-                <div 
-                  class="text-xs font-light mt-1"> 
-                  <div class="grid grid-cols-12"                  
+                <div
+                  class="text-xs font-light mt-1">
+                  <div class="flex gap-2"
                     classList={{
                       ' text-orange-500 dark:text-orange-200 ' : props.classValidation === 1,
                       ' text-pink-600 dark:text-pink-200 ' : props.classValidation === 2,
                     }} >
                     <Switch>
                       <Match when={props.classValidation === 1}>
-                        <div class="col-span-1 flex justify-center items-start">
+                        <div class="flex justify-center items-start shrink-0">
                           <svg xmlns="http://www.w3.org/2000/svg" class="h-4 w-4" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
                             <path stroke-linecap="round" stroke-linejoin="round" d="M12 9v2m0 4h.01m-6.938 4h13.856c1.54 0 2.502-1.667 1.732-3L13.732 4c-.77-1.333-2.694-1.333-3.464 0L3.34 16c-.77 1.333.192 3 1.732 3z" />
                           </svg>
                         </div>
                       </Match>
                       <Match when={props.classValidation === 2}>
-                        <div class="col-span-1 flex justify-center items-start">
+                        <div class="flex justify-center items-start shrink-0">
                           <svg xmlns="http://www.w3.org/2000/svg" class="h-4 w-4" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
                             <path stroke-linecap="round" stroke-linejoin="round" d="M12 8v4m0 4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z" />
                           </svg>
                         </div>
                       </Match>
                     </Switch>
-                    <div class="col-span-11 text-justify mr-1" innerHTML={item}/>
+                    <div class="flex-1 text-justify" innerHTML={item}/>
                   </div>
                 </div>
               )}
@@ -110,7 +106,7 @@ const TextAreaInput: FormComponentBase = props => {
           </Show>
         </div>
         <Show when={enableRemark()}>
-          <div class=" flex justify-end ">           
+          <div class="shrink-0">
               <button class="relative inline-block bg-white p-2 h-10 w-10 text-gray-500 rounded-full  hover:bg-yellow-100 hover:text-yellow-400 hover:border-yellow-100 border-2 border-gray-300 disabled:bg-gray-200 dark:disabled:bg-gray-700 dark:disabled:text-gray-400"
                 disabled = { disableClickRemark() }
                 onClick={e => props.openRemark(props.component.dataKey)}>
